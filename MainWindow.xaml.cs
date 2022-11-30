98e38033fa8e65536fb5faecdfab50a101d3d6b7
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2022-11-29 21:13:26 -0800

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,13 +23,109 @@ namespace Lecture_17_Prog_122_F22
 
     public partial class MainWindow : Window
     {
+        List<CheckBox> checkboxes = new List<CheckBox>();
+
+        // Change My List to a different type of collection
+        //List<User> users = new List<User>();
+
+        ObservableCollection<User> users = new ObservableCollection<User>();
 
         public MainWindow()
         {
             InitializeComponent();
-         
+
+            Preload();
+
+            lvUsers.ItemsSource = users;
+            lbNumbers.ItemsSource = users;
         }
 
+        public void Preload()
+        {
+            checkboxes.Add(chkPepperoni);
+            checkboxes.Add(chkMushroom);
+            checkboxes.Add(chkOlives);
+            //PopulateBoxes();
+
+            users.Add(new User()
+            {
+                Name = "Will",
+                Program = "CSI"
+            });
+
+            users.Add(new User()
+            {
+                Name = "Wong",
+                Program = "CSI"
+            });
+
+            users.Add(new User()
+            {
+                Name = "Zack",
+                Program = "CSI"
+            });
+
+            users.Add(new User()
+            {
+                Name = "Hannah",
+                Program = "CSI"
+            });
+        }
+
+        private void btnDisplayName_Click(object sender, RoutedEventArgs e)
+        {
+            //DisplayCheckBoxInformation();
+            users.Add(new User()
+            {
+                Name = "Sam",
+                Program = "CSI"
+            });
+
+        }
+
+        public void DisplayName()
+        {
+            string fName = txtFName.Text;
+            string lName = txtLName.Text;
+
+            rtbDisplay.Text = $"{fName} {lName}";
+        } // DisplayName
+
+        // Pepperoni = true
+        // Mushroom = false
+        // Olives = true
+        public void DisplayCheckBoxInformation()
+        {
+            rtbDisplay.Text = "";
+            foreach (CheckBox box in checkboxes)
+            {
+                rtbDisplay.Text += $"{box.Content} = {box.IsChecked}\n";
+
+            }
+
+        }
+
+        public void PopulateBoxes()
+        {
+            for (int i = 0; i <= 20; i++)
+            {
+                lbNumbers.Items.Add(i);
+            }
+        }
+
+        // Check Box
+        // Radio Button
+        // ListBox
+        // Combo Box
+
+        // ListView
+
+    } // class
+
+    public class User
+    {
+        public string Name { get; set; }
+        public string Program { get; set; }
     }
 
 }
