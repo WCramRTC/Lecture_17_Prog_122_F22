3e0ac2fbbdcd4de3594ea9358fef01a5aac957f5
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2022-11-29 18:28:33 -0800

    Added New Form

--- NewForm.xaml.cs
+++ NewForm.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,30 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Lecture_17_Prog_122_F22
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for NewForm.xaml
     /// </summary>
-    /// 
-
-    public partial class MainWindow : Window
+    public partial class NewForm : Window
     {
-
-    
-
-        public MainWindow()
+        public NewForm()
         {
             InitializeComponent();
-         
-
         }
-
-    
     }
-
-
 }
